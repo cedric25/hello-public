46035588b1ea5e3b6040abd794caf8bf3aaab43c
Cedric Nicoloso <cnicoloso@octo.com>
2016-04-28 10:57:50 +1000

    /getHello endpoint

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+/**
+ * --- HOME ---
+ */
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Home');
+  }
+});
+
+/**
+ * --- HELLO ---
+ */
+server.route({
+  method: 'GET',
+  path: '/getHello',
+  handler: function (request, reply) {
+    reply('Hello!');
+  }
+});
+
+// server.register([], (err) => {
+server.start((err) => {
+  console.log('Server running at:', server.info.uri);
+});
+
